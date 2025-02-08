00832d649b7e904c7d734376cd6cbc5f3958787a
MonsterGuti <gogulanovmartin@gmail.com>
2025-02-08 11:36:53 +0200

    Update decipher_this.py

--- list_advanced_exercise/decipher_this.py
+++ list_advanced_exercise/decipher_this.py
@@ -1,1 +1,17 @@
 user_string = input().split()
+filtered_list = []
+for word in user_string:
+    digit_chars = ""
+    len_of_digits = 0
+    for char in word:
+        if char.isdigit():
+            digit_chars += char
+            len_of_digits += 1
+    word = chr(int(digit_chars)) + word[len_of_digits:]
+    char_list = [char for char in word]
+    char_list[1], char_list[-1] = char_list[-1], char_list[1]
+    final_word = "".join(char_list)
+    filtered_list.append(final_word)
+    char_list = []
+
+print(" ".join(filtered_list))
